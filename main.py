dc1d600df652918e056c09f24c262415b83dfbbf
josephAlonso7 <103160385+josephAlonso7@users.noreply.github.com>
2025-09-29 20:41:16 -0600

    carga inicial

---
+++ main.py
@@ -0,0 +1,33 @@
+suma = 6 +8 +6-4-3+23+5-1-5-99+100-121212-54646\
+       -23+65474564\
+       +1\
+       -2
+
+cadena = "a"\
+           "b" 
+
+#print(cadena)
+
+
+arreglo= [1,2,3,4,5,6]
+
+#print(arreglo)
+
+
+'''
+hola
+esto será ignorado 
+:)
+'''
+# Funcion para sumar
+def suma(a, b):
+    suma = a + b
+    print(suma)
+
+suma(3,40)
+
+
+
+
+
+
